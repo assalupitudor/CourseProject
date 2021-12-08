97296e9631f734e49c312ec6600a2753ef30f4d4
assalupitudor <55034284+assalupitudor@users.noreply.github.com>
2021-12-07 19:05:58 -0600

    made code into functions

--- keyword_extraction.py
+++ keyword_extraction.py
@@ -12,43 +12,41 @@ Model Name should ALWAYS be 'ner'; Added modifiable model name so future updates
 Enter sample sentence, gives keyword and keywork type
 '''
 
+def extract(file_path, label_path):
+    file = open(file_path, 'r', encoding='cp1252')
+    doc = file.read()
+    # read the label data
+    labels = pd.read_csv(label_path)
+    word_dict = labels.set_index('entities')['labels'].to_dict() #initialize this with labels
 
-file_path = 'data.txt'
-file = open(file_path, 'r', encoding='cp1252')
-doc = file.read()
-# read the label data
-labels = pd.read_csv('labels.csv')
-word_dict = labels.set_index('entities')['labels'].to_dict() #initialize this with labels
+    #using eglish words create sentencizer to process the document/file
+    process = English()
+    process.add_pipe('sentencizer') 
+    doc = process(doc)
+    sentences = [sentence.text.strip() for sentence in doc.sents]
 
-#using eglish words create sentencizer to process the document/file
-process = English()
-process.add_pipe('sentencizer') 
-doc = process(doc)
-sentences = [sentence.text.strip() for sentence in doc.sents]
-
-data = []
-#corpus builder
-for sentence in sentences:
-    #replace
-    word_list = re.sub("[^\w]", " ",  sentence).split()
-    for word in word_list:
-        d = {}
-        l = []
-        if word in word_dict:
-            start_word = sentence.find(word)
-            end_word = start_word+len(word)
-            label = word_dict.get(word)
-            list_t = (start_word,end_word,label)
-            l.append(list_t)
-            d['entities'] = l
-            data_t = (sentence,d)
-            data.append(data_t)
-
+    data = []
+    #corpus builder
+    for sentence in sentences:
+        #replace
+        word_list = re.sub("[^\w]", " ",  sentence).split()
+        for word in word_list:
+            d = {}
+            l = []
+            if word in word_dict:
+                start_word = sentence.find(word)
+                end_word = start_word+len(word)
+                label = word_dict.get(word)
+                list_t = (start_word,end_word,label)
+                l.append(list_t)
+                d['entities'] = l
+                data_t = (sentence,d)
+                data.append(data_t)
+    return data
 
 def train(data, itr):
     #create a blank language class
     nlp = spacy.blank('en')
-    # name enyry recognition 
     if 'ner' not in nlp.pipe_names:
         ner = nlp.create_pipe('ner')
         nlp.add_pipe('ner', last=True)
@@ -79,6 +77,7 @@ def train(data, itr):
 
 
 #Get number of iterations as input
+data = extract('data.txt', 'labels.csv')
 iterations_input = int(input("Enter number of iterations: "))
 prdnlp = train(data, iterations_input)
 
